971eab2039d875f99ea23774b262cefbc42ccea4
Suthan kathir <suthancode2gmail.com>
2022-01-31 23:48:04 -0500

    delete dulpicated

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Import the routes
 const routes = require('./controllers');
@@ -7,13 +10,23 @@ const routes = require('./controllers');
 const connection = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
+// const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 // Turn on routes
 app.use(routes);
+
+// app.engine('handlebars');
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 // Turn on connection to DB and server
 // {force: true} forces MySQL to recreate tables if there are association changes with them.
